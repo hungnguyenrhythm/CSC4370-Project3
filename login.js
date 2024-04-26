0dbf8db19ecf559ea49020e46aae83bedec9396a
Leah Cheong <143969209+leahcheong@users.noreply.github.com>
2024-04-26 15:31:05 -0400

    Delete login.js

--- login.js
+++
@@ -1,9 +0,0 @@
-function switchToSignUp(){
-    document.getElementById("signupForm").style.display = "block";
-    document.getElementById("loginForm").style.display = "none";
-}
-
-function switchToLogIn(){
-    document.getElementById("signupForm").style.display = "none";
-    document.getElementById("loginForm").style.display = "block";
-}
